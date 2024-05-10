9aa2ada539b93a78665d39506c22a8a74e64b7c0
Yuriy Seniuk <yoursen@gmail.com>
2024-05-10 14:22:04 +0300

    Added IsAnswered property

--- WordFinderDB/DB/GameWord.cs
+++ WordFinderDB/DB/GameWord.cs
@@ -5,6 +5,7 @@ public class GameWord
     public string Word { get; set; }
     public string Description { get; set; }
     public bool IsPlayed { get; set; }
+    public bool IsAnswered { get; set; }
     public GameWordCategory Category { get; set; }
     public ComplexityType Complexity { get; set; }
 }
